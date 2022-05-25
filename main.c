fef7bbb62358f72086d1d2bf7a3e577415677cd8
luna-vyer <104188878+luna-vyer@users.noreply.github.com>
2022-05-25 16:03:04 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
 
 int main (void) {
-    return EXIT_SUCCESS;
+    HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString ("123456789012345678901234567890");
+    printHugeUnsignedInt (hugeNumber);
+    hugeNumber = createHugeUnsignedIntFromString ("0000000123456789012345678901234567890");
+    printHugeUnsignedInt (hugeNumber);
 }
+
